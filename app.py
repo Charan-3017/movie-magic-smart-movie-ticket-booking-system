6246e1c858a94cc308be8d4d056ccdc566225194
Charan-3017 <charanjambugolam@gmail.com>
2025-07-04 16:50:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,8 @@ sns = boto3.client('sns', region_name='us-east-1')
 users_table = dynamodb.Table('MovieMagic_Users')
 bookings_table = dynamodb.Table('MovieMagic_Bookings')
 
-SNS_TOPIC_ARN = 'arn:aws:sns:us-east-1:841162686181:MovieShows'  # Replace this
+SNS_TOPIC_ARN = 'arn:aws:sns:us-east-1:841162686181:MovieShows'  # Replace this with your topic ARN
 
-# Movie list
 movies = [
     {"id": 2, "name": "Avengers: Endgame", "time": "6:00 PM", "price": 150, "rating": 4.8, "image": "avengers.jpeg"},
     {"id": 7, "name": "Leo", "time": "3:30 PM", "price": 180, "rating": 4.7, "image": "leo.jpeg"},
@@ -98,7 +97,7 @@ def book_ticket(id):
             booking_id = f"BK{random.randint(1000,9999)}"
             bookings_table.put_item(Item={
                 'Bookingid': booking_id,
-                'Email': session['user'],
+                'email': session['user'],  # ✅ fixed key
                 'Movie': movie['name'],
                 'User': name,
                 'Tickets': tickets
